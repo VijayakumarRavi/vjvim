4a0bc0342e32aa2dba9ba6984952dcb0e18f0e51
Vijayakumar Ravi <me@vijayakumar.xyz>
2023-12-22 19:08:12 +0530

    🚧 build(Cachix): some cache things
    Signed-off-by: Vijayakumar Ravi <me@vijayakumar.xyz>

--- flake.nix
+++ flake.nix
@@ -1,6 +1,17 @@
 {
   description = "Vijay's Neovim (vjvim) Configuration";
-  
+  nixConfig = {
+    extra-substituters = [
+      "https://cache.nixos.org"
+      "https://nix-community.cachix.org"
+      "https://vjvim.cachix.org"
+    ];
+    extra-trusted-public-keys = [
+      "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+      "vjvim.cachix.org-1:AF3grdItpExuZ95D16gb7DN/9kYhf91OwZoNBCfHW98="
+    ];
+  };
   inputs = {
     nixpkgs = {
       url = "github:NixOS/nixpkgs";
