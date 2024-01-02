f6f6e7cb17004173c504e5ab9d1b5112b9a0d66d
Vijayakumar Ravi <me@vijayakumar.xyz>
2024-01-03 00:26:03 +0530

    🧼 correction(formatting): code formatting
    Signed-off-by: Vijayakumar Ravi <me@vijayakumar.xyz>

--- packages/vjvim.nix
+++ packages/vjvim.nix
@@ -3,8 +3,8 @@ let
   customRC = import ../config { inherit pkgs; };
   plugins = import ../plugins.nix { inherit pkgs; };
 in pkgs.wrapNeovim pkgs.neovim {
-      configure = {
-        inherit customRC;
-        packages.all.start = plugins;
-    };
+  configure = {
+    inherit customRC;
+    packages.all.start = plugins;
+  };
 }
